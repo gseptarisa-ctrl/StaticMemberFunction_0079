af42e640440415b71f09953f6b0a400c4442f7ce
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-05-05 17:08:02 +0700

    membuat constractor berparameter serta fungsinya

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,15 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); // constructor dengan parameter
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main() {
+    mahasiswa mhs(113, "Rizqi Tukang Makan"); // memanggil constructor dengan parameter
+    return 0;
+}
